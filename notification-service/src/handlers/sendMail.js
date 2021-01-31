bf75b0fef596da1f23adf310954b22fd5d42e21d
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-31 17:25:15 +1000

    Manually sent a message from SQS console

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -4,20 +4,27 @@ import AWS from "aws-sdk";
 const ses = new AWS.SES({ region: "ap-southeast-2" });
 
 async function sendMail(event, context) {
+  // grab SQS message (string) from event.Records
+  const record = event.Records[0]; // the 1 message being processed at a time (batchSize: 1)
+  console.log("record processing", record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   // Prepare parameters
   const params = {
     Source: "joe.cui@outlook.com", // verified email in aws ses
     Destination: {
-      ToAddresses: ["xiaozhou.cui@gmail.com"],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: "Hello from Joe",
+          Data: body,
         },
       },
       Subject: {
-        Data: "Test Mail",
+        Data: subject,
       },
     },
   };
