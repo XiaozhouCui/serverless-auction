5d9f43303a8bd4e78fa39459e386db29a401b215
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-01 19:50:36 +1000

    Notify seller and bidder when closing an auction

--- auction-service/src/lib/closeAuction.js
+++ auction-service/src/lib/closeAuction.js
@@ -1,6 +1,8 @@
 import AWS from "aws-sdk";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+// send SQS notice when auction is closed
+const sqs = new AWS.SQS();
 
 // arg "auction" is the auction item fetched from dynamodb
 export async function closeAuction(auction) {
@@ -16,6 +18,36 @@ export async function closeAuction(auction) {
     },
   };
 
-  const result = await dynamodb.update(params).promise();
-  return result;
+  // close auction
+  await dynamodb.update(params).promise();
+
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  // send sqs message to seller
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "Your item has been sold!",
+        recipient: seller,
+        body: `Woohoo! Your item "${title}" has been sold for $${amount}.`,
+      }),
+    })
+    .promise();
+
+  // send sqs message to bidder
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "You won an auction!",
+        recipient: bidder,
+        body: `What a great deal! You got yourself a "${title}" for $${amount}.`,
+      }),
+    })
+    .promise();
+
+  // Wait for SQS to send both messages
+  return Promise.all([notifySeller, notifyBidder]);
 }
