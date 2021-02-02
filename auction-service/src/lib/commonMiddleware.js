9d93c166277b03fc120c8fda261250ea23b2609b
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-02 14:39:33 +1000

    Added CORS for frontend app

--- auction-service/src/lib/commonMiddleware.js
+++ auction-service/src/lib/commonMiddleware.js
@@ -2,6 +2,7 @@ import middy from "@middy/core";
 import httpJsonBodyParser from "@middy/http-json-body-parser";
 import httpEventNormalizer from "@middy/http-event-normalizer";
 import httpErrorHandler from "@middy/http-error-handler";
+import cors from "@middy/http-cors";
 
 // wrap handler functions with middy, in order to add .use() method
 export default (handler) =>
@@ -9,4 +10,5 @@ export default (handler) =>
     httpJsonBodyParser(), // auto parse JSON string in event arg
     httpEventNormalizer(), // adjust the API Gateway event objects to prevent errors caused by non-existing objects
     httpErrorHandler(),
+    cors(),
   ]);
