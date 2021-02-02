e7c64604b267bb37db42b26c01bf1e7544a139cc
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-02 11:41:47 +1000

    Added js files to upload base64 pictures to S3

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,18 @@
+import { getAuctionById } from "./getAuction";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+
 export async function uploadAuctionPicture(event) {
+  // get auction id
+  const { id } = event.pathParameters;
+  // get auction object
+  const auction = await getAuctionById(id);
+  // handle picture file
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
+
+  const uploadToS3Result = await uploadPictureToS3(auction.id + ".jpg", buffer);
+  console.log(uploadToS3Result);
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
---
+++ auction-service/src/lib/uploadPictureToS3.js
@@ -0,0 +1,19 @@
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3();
+
+export async function uploadPictureToS3(key, body) {
+  // arg "key" is the object key in S3, arg "body" is the actual data buffer
+  const result = await s3
+    .upload({
+      // S3 parameters
+      Bucket: process.env.AUCTIONS_BUCKET_NAME,
+      Key: key,
+      Body: body,
+      ContentEncoding: "base64",
+      ContentType: "image/jpeg",
+    })
+    .promise();
+
+  return result;
+}
